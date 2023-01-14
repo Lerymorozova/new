c04218fcf27a47d288b171b1089262db6cd0e0a8
valerya <reykon-kun@yandex.ru>
2023-01-14 23:31:09 +0200

    less5

--- helloconcole/Program.cs
+++ helloconcole/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine(" 2Hello, World! ");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[4];
+Random rand = new Random();
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(234, 897);
+    Console.Write($"{array[i]} ");
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine($"{result} ");
